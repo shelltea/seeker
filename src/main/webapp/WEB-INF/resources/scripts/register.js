b70ed80227702e263cd1a500b4de5c3edb1a5847
xiongshuhong <shelltea@gmail.com>
2013-10-09 09:50:47 +0800

    更新jetty至9.0.6.v20130930，nice Validator至0.5.0

--- src/main/webapp/WEB-INF/resources/scripts/register.js
+++ src/main/webapp/WEB-INF/resources/scripts/register.js
@@ -1,7 +1,6 @@
 $(function() {
 	$('#registerForm').validator({
-		theme : 'yellow_right_effect',
-		stopOnError : false,
+		theme : 'yellow_right',
 		fields : {
 			'email' : 'required;email;remote[get:' + contextPath + '/api/accounts/checking/email]',
 			'username' : 'required;username;remote[get:' + contextPath + '/api/accounts/checking/username]',
