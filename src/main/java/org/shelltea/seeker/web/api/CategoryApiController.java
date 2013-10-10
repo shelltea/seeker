8090160082fd59d388482a66d0d6a55e1206c391
xiongshuhong <shelltea@gmail.com>
2013-10-11 00:07:42 +0800

    增加用户订阅Feed的功能

--- src/main/java/org/shelltea/seeker/web/api/CategoryApiController.java
+++ src/main/java/org/shelltea/seeker/web/api/CategoryApiController.java
@@ -4,7 +4,11 @@
 package org.shelltea.seeker.web.api;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.shelltea.seeker.entity.Category;
+import org.shelltea.seeker.entity.Feed;
 import org.shelltea.seeker.repository.CategoryRepository;
+import org.shelltea.seeker.repository.FeedRepository;
 import org.shelltea.seeker.service.CategoryService;
 import org.shelltea.seeker.web.api.entity.Response;
 import org.shelltea.seeker.web.entity.ShiroAccount;
@@ -12,6 +16,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +32,33 @@ public class CategoryApiController {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private FeedRepository feedRepository;
+
+	@ResponseBody
+	@RequestMapping(value = "/{id}", method = RequestMethod.POST)
+	public Response addFeedToCategory(@PathVariable long id, @RequestBody Feed feed) {
+		feed = feedRepository.findOne(feed.getId());
+
+		if (feed == null) {
+			return new Response();
+		}
+
+		// 验证用户是否有添加Feed到此Category的权限
+		Subject currentUser = SecurityUtils.getSubject();
+		if (currentUser.isPermitted("categories:add-feed:" + id)) {
+			Category category = categoryRepository.findOne(id);
+
+			if (category == null) {
+				return new Response();
+			}
+
+			category.getFeeds().add(feed);
+			return new Response(categoryRepository.save(category));
+		} else {
+			return new Response();
+		}
+	}
 
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.GET)
--- src/main/webapp/WEB-INF/resources/scripts/explore.js
+++ src/main/webapp/WEB-INF/resources/scripts/explore.js
@@ -1,12 +1,28 @@
 $(function() {
+	var rootCategoryId;
+
 	// 计算Tab高度
 	var tabsDivHeight = $(window).height() - ($('#navbar').height() + $('#localebar').height());
 	$('#channel-tabs').height(tabsDivHeight - 25);
 
+	// 获取根目录
+	$.get(contextPath + '/api/categories', function(data) {
+		rootCategoryId = data.data.id;
+	});
+
+	// 获取频道和订阅源
 	$.get(contextPath + '/api/channels', function(data) {
 		var template = Handlebars.compile($('#channel-template').html());
 		var html = template(data);
-		console.log(html);
 		$('#channel-tabs').html(html);
+	});
+
+	// 订阅
+	$('#channel-tabs').on('click', 'span[id^="feed-"]', function() {
+		$.post(contextPath + '/api/categories/' + rootCategoryId, JSON.stringify({
+			id : $(this).data('id')
+		}), function(data) {
+			console.log(data);
+		});
 	});
 });
