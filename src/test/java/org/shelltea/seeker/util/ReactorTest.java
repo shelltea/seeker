0ed4b2d716cd7edb4cea49698334904dd729e7a7
shelltea <shelltea@gmail.com>
2014-12-27 23:52:48 +0800

    更新Reactor单元测试

--- src/test/java/org/shelltea/seeker/util/ReactorTest.java
+++ src/test/java/org/shelltea/seeker/util/ReactorTest.java
@@ -5,12 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.Environment;
-import reactor.core.Reactor;
-import reactor.core.spec.Reactors;
-import reactor.event.Event;
-import reactor.event.selector.Selectors;
-import reactor.function.Consumer;
+import reactor.Environment;
 import reactor.rx.Promise;
 import reactor.rx.Promises;
 
@@ -30,42 +25,17 @@ public class ReactorTest {
 
     @Test
     public void test1() {
-        Reactor reactor = Reactors.reactor().env(environment).dispatcher(Environment.RING_BUFFER).get();
+        Promise<String> promise = Promises.prepare(environment);
 
-        reactor.on(Selectors.$("test"), new Consumer<Event<String>>() {
-            @Override
-            public void accept(Event<String> event) {
-                logger.debug("accept:{}", event.getData());
-            }
-        });
-
-        reactor.notify("test", Event.wrap("data"));
-    }
-
-    @Test
-    public void test2() {
-        Promise<String> promise = Promises.defer(environment);
-
-        promise.onComplete(new Consumer<Promise<String>>() {
-            @Override
-            public void accept(Promise<String> stringPromise) {
-                logger.debug("length:{}", stringPromise.get());
-
-            }
-        });
+        promise.onComplete(stringPromise -> logger.debug("length:{}", stringPromise.get()));
 
         promise.accept(RandomStringUtils.randomAlphanumeric(5));
     }
 
     @Test
-    public void test3() {
+    public void test2() {
         Promise<String> promise = Promises.success(environment, "success");
 
-        promise.onComplete(new Consumer<Promise<String>>() {
-            @Override
-            public void accept(Promise<String> stringPromise) {
-                System.out.println(1 + stringPromise.get());
-            }
-        });
+        promise.onComplete(stringPromise -> logger.debug("{}", stringPromise.get()));
     }
 }
